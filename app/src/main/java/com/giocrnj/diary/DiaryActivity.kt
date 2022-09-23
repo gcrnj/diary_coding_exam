0f0b8de26192fe8694925575e9c376d4f050b965
giocornejo <gio.cornejo@stationfive.com>
2022-09-23 21:57:25 +0800

    Layout Implementation

--- app/src/main/java/com/giocrnj/diary/DiaryActivity.kt
+++ app/src/main/java/com/giocrnj/diary/DiaryActivity.kt
@@ -2,10 +2,24 @@ package com.giocrnj.diary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.giocrnj.diary.databinding.ActivityMainBinding
 
 class DiaryActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding                  //This activity's binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)   //set binding
+        setContentView(binding.root)                            //set layout
+
+        initiateClickListeners()
+
+
+    }
+
+    private fun initiateClickListeners() {
+
+
+
     }
 }
